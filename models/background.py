ef7bd2350a965e67552235debeee52be4cd4df9d
MikaSie <mikasie6@gmail.com>
2025-02-19 10:40:34 +0100

    update

--- models/background.py
+++ models/background.py
@@ -1,18 +1,15 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+#TODO: Add relative paths
 
 class background_creator:
 
-    def __init__(self, background):
+    def __init__(self):
         pass
-
-
-
-    def create_background(self, background):
-        pass
-
-
 
 
 if __name__ == '__main__':
     background_creator = background_creator()
-    
+
--- models/segmentation.py
+++ models/segmentation.py
@@ -2,6 +2,9 @@ import rembg
 import matplotlib.pyplot as plt
 from PIL import Image
 
+#TODO: Add relative paths
+
+
 class Segmenter:
     def __init__(self, model_name: str = 'u2net_human_seg'):
         self.model = self._load_model(model_name)
@@ -12,6 +15,28 @@ class Segmenter:
         session = rembg.new_session(model_name= model_name)
         return session
     
+
+    def create_background(self, background_path:str, foreground_path: str = 'tests/output.png'):
+        """
+        Creates a background for the given image using the model and saves the output.
+
+        Args:
+            background (str): The path to the background image file.
+            foreground_path (str): The path to the segmented image file.
+
+        Returns:
+            file object: The file object of the output image.
+
+        Raises:
+            FileNotFoundError: If the image file does not exist.
+            IOError: If there is an error reading or writing the files.
+        """
+        foreground = Image.open(foreground_path)
+        background = Image.open(background_path)
+        background.paste(foreground, (0, 0), foreground)
+        background.save('tests/complete_image.png')
+        return background
+
 
     def segment(self, image_path: str):
         """
@@ -28,11 +53,20 @@ class Segmenter:
             IOError: If there is an error reading or writing the files.
         """
         print(f'Segmenting image {image_path}')
-        with open(image_path, 'rb') as img_file:
-            with open('tests/output.png', 'wb') as output_file:
-                input = img_file.read()
-                output = rembg.remove(input, session= self.model)
-                output_file.write(output)
+        alpha_matting_tests = [0, 50, 100, 150, 200, 250, 255]
+        for i in range(7):
+            print(f'Running test {i}')
+            with open(image_path, 'rb') as img_file:
+                with open(f'tests/output{i}.png', 'wb') as output_file:
+                    input = img_file.read()
+                    output = rembg.remove(input, session= self.model, alpha_matting= True, alpha_matting_erode_size= alpha_matting_tests[i])
+                    output_file.write(output)
+
+
+        """plt.imshow(Image.open('tests/output.png'))
+        plt.axis('off')
+        plt.show()"""
+        
         return output_file
     
 
@@ -41,7 +75,8 @@ if __name__ == '__main__':
     segmenter = Segmenter()
     test_path = 'tests/mika.jpeg'
     segmenter.segment(test_path)
-    
-    plt.imshow(Image.open('tests/output.png'))
-    plt.axis('off')
-    plt.show()
+    #segmenter.create_background(background_path='tests/background.jpeg', foreground_path='tests/output.png')
+
+    #plt.imshow(Image.open('tests/complete_image.png'))
+    #plt.axis('off')
+    #plt.show()
